871e37dbe315350fdf5d9d400bca994b9362d60a
Junpark2889 <jhpark2888@naver.com>
2022-11-24 12:25:56 +0900

    저장1

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,12 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 콘솔 실행 : ctrl + F11
+		// 자동 포맷 정렬 : ctrl + shift + f 
+		System.out.println("Hello 이상용 자바"); 
+	}
+
+}
